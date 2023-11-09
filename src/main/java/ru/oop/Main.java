a9d252991e92ed270b1c62ae184e2e4691b6cbda
Danil But <danil.but.1998@Gmail.com>
2023-11-09 23:52:44 +0500

    Добавил решение 2 задачи

--- src/main/java/ru/oop/Main.java
+++ src/main/java/ru/oop/Main.java
@@ -36,6 +36,18 @@ import java.util.List;
 public class Main {
 
     /**
+     *Добраться до заданной точки на любом виде транспорта
+     */
+
+    public static void moveTo2(Person person, Position destination, Transport transport) {
+        if(person.getPosition() != transport.getPosition())
+            person.walk(transport.getPosition());
+        transport.moveTo(person);
+        if(person.getPosition() != destination)
+            person.walk(destination);
+    }
+
+    /**
      * Мы получаем список транспорта на котором нам<br>
      * необходимо доехать до конечной точки.<br>
      * Проверяем нужно ли нам идти пешком до 1 в списке транспорта,<br>
@@ -45,15 +57,16 @@ public class Main {
      * Если нет, то идем до конца пешком.<br>
      *
      */
-    public static void moveTo(Person person, Position destination) {
-
+    public static void moveTo3(Person person, Position destination) {
         Transfers transfers = new Transfers();
         List<Transport> path = transfers.getPathList(person, destination);
-        if(path.get(0).getPosition() != person.getPosition()) person.walk(path.get(0).getPosition());
+        if(path.get(0).getPosition() != person.getPosition())
+            person.walk(path.get(0).getPosition());
         for(Transport transport: path){
             transport.moveTo(person);
         }
-        if(person.getPosition() != destination) person.walk(destination);
+        if(person.getPosition() != destination)
+            person.walk(destination);
         assert person.getPosition() == destination;
     }
 }
